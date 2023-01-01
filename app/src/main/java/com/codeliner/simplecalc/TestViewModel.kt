f8f2c5e6be9612bf8558cef8647df798e1a09e7e
Your Name <you@example.com>
2023-01-01 22:49:52 +0300

    сделал тоже самое,только на основе вьюмодели

---
+++ app/src/main/java/com/codeliner/simplecalc/TestViewModel.kt
@@ -0,0 +1,19 @@
+package com.codeliner.simplecalc
+
+import androidx.lifecycle.ViewModel
+
+class TestViewModel : ViewModel() {
+
+    var counterValue = 0
+
+    fun updateCount() {
+        counterValue++
+        if (counterValue > 10) counterValue = 10
+    }
+
+    fun decreaseCount() {
+        counterValue--
+        if (counterValue < 0) counterValue = 0
+    }
+
+}
