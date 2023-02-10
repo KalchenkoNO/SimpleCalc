dd084a4844fa739cbaa0b25fe6b75574cf6e00f3
Nikita Novgorodtsev <novgorodcevn5@gmail.com>
2023-02-10 16:58:52 +0300

    Добавил ливдвту,убрал лишние пробелы

--- app/src/main/java/com/codeliner/simplecalc/TestViewModel.kt
+++ app/src/main/java/com/codeliner/simplecalc/TestViewModel.kt
@@ -1,19 +1,29 @@
 package com.codeliner.simplecalc
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.liveData
 
-class TestViewModel : ViewModel() {
+class ActivityViewModel : ViewModel() {
+    private var counterValue = 0
+    private val counterValueLiveData = liveData<Int> {}
 
-    var counterValue = 0
+    fun getCounterValueLiveData(): LiveData = counterLiveData
 
-    fun updateCount() {
+    fun onButtonClick(buttonId: Int) {
+        when (buttonId) {
+            R.id.button_increase -> updateCount()
+            R.id.button_decrease -> decreaseCount()
+            R.id.button_clear -> counterValue = 0
+        }
+    }
+    private fun updateCount() {
         counterValue++
         if (counterValue > 10) counterValue = 10
     }
-
-    fun decreaseCount() {
+    private fun decreaseCount() {
         counterValue--
         if (counterValue < 0) counterValue = 0
     }
-
 }
